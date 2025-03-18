0cf0c394d4f5d18717d2000b1ac21fdb12ea5c62
AmSingh2006 <amsingh2006@icloud.com>
2025-03-18 15:20:38 +0000

    gg

--- Main.py
+++ Main.py
@@ -1,4 +1,7 @@
 try:
     import simplegui
-except ImportError:
+except ImportError: 
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+
+sprite_sheet_url = "gamesprite.png"  
+sprite_sheet = simplegui.load_image(sprite_sheet_url)
